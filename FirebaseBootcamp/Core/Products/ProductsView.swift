62bd63976f7ae3436ddaccdc273f8bc7c9951789
Sagar Jangra <sagarjangra767@gmail.com>
2026-07-12 03:06:34 +0530

    Created indexes in firebase for fast query using multiple option together

--- FirebaseBootcamp/Core/Products/ProductsView.swift
+++ FirebaseBootcamp/Core/Products/ProductsView.swift
@@ -12,11 +12,58 @@ import Combine
 final class ProductsViewModel: ObservableObject {
     
     @Published private(set) var products: [Product] = []
+    @Published var selectedFilter: FilterOption? = nil
+    @Published var selectedCategory: CategoryOption? = nil
     
-    func getAllProducts() async throws {
-        self.products = try await ProductsManager.shared.getAllProducts()
+//    func getAllProducts() async throws {
+//        self.products = try await ProductsManager.shared.getAllProducts()
+//    }
+    
+    // MARK: Filter
+    enum FilterOption: String, CaseIterable {
+        case noFilter
+        case priceHigh
+        case priceLow
+        
+        var priceDescending: Bool? {
+            switch self {
+            case .noFilter: nil
+            case .priceHigh: true
+            case .priceLow: false
+            }
+        }
     }
-
+    
+    func filterSelected(option: FilterOption) async throws {
+        self.selectedFilter = option
+        try await self.getProducts()
+    }
+    
+    // MARK: Category
+    enum CategoryOption: String, CaseIterable {
+        case noCategory
+        case furniture
+        case beauty
+        case fragrances
+        case groceries
+        
+        var categoryKey: String? {
+            if self == .noCategory {
+                return nil
+            }
+            return self.rawValue
+        }
+    }
+    
+    func categorySelected(option: CategoryOption) async throws {
+        self.selectedCategory = option
+        try await self.getProducts()
+    }
+    
+    func getProducts() async throws {
+        self.products = try await ProductsManager.shared.getAllProducts(priceDescending: selectedFilter?.priceDescending, forCategory: selectedCategory?.categoryKey)
+    }
+    
 }
 
 struct ProductsView: View {
@@ -28,12 +75,39 @@ struct ProductsView: View {
             }
         }
         .navigationTitle("Products")
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarLeading) {
+                Menu("Filter: \(vm.selectedFilter?.rawValue ?? "None")") {
+                    ForEach(ProductsViewModel.FilterOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await vm.filterSelected(option: option)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Menu("Category: \(vm.selectedCategory?.rawValue ?? "None")") {
+                    ForEach(ProductsViewModel.CategoryOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await vm.categorySelected(option: option)
+                            }
+                        }
+                    }
+                }
+            }
+        })
         .task {
-            try? await vm.getAllProducts()
+            try? await vm.getProducts()
         }
     }
 }
 
 #Preview {
-    ProductsView()
+    NavigationStack {
+        ProductsView()
+    }
 }
